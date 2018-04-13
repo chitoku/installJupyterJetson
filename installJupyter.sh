215f4cccbf146a2fc2bc0990e5e91c009732e78d
Chitoku YATO <chitoku@gmail.com>
2018-04-13 22:00:11 +0000

    Add installJupyter.sh script.

---
+++ installJupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install python-pip
+
+# upgrade pip
+pip install -U pip
+pip --version	# pip 9.0.1 from /home/ubuntu/.local/lib/python2.7/site-packages (python 2.7)
+
+sudo apt-get install -y ipython ipython-notebook
+sudo pip install jupyter
