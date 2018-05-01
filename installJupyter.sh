93f4c44a80aa6f58a3a8099f0e3f22052b97cb90
Chitoku YATO <chitoku@gmail.com>
2018-05-01 17:49:06 +0000

    Pin PIP version to 9.0.3.

--- installJupyter.sh
+++ installJupyter.sh
@@ -3,8 +3,8 @@
 sudo apt-get install -y python-pip
 
 # upgrade pip
-pip install -U pip
+pip install --upgrade pip==9.0.3
 pip --version	# pip 9.0.1 from /home/ubuntu/.local/lib/python2.7/site-packages (python 2.7)
 
 sudo apt-get install -y ipython ipython-notebook
-sudo pip install jupyter
+sudo -H pip install jupyter
