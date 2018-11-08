de5e12032bab76cd5806e1802a7d0683e4cef7f1
Chitoku YATO <chitoku@gmail.com>
2018-11-08 17:56:57 +0000

    Take care of libzmq issue

--- setupJupyter.sh
+++ setupJupyter.sh
@@ -6,7 +6,7 @@ JUPYTER_CONF=$HOME"/.jupyter/jupyter_notebook_config.py"
 
 cp $JUPYTER_CONF $JUPYTER_CONF".orig"
 
-echo "c.NotebookApp.ip = '*'" >> $JUPYTER_CONF
+echo "c.NotebookApp.ip = '0.0.0.0'" >> $JUPYTER_CONF
 echo "c.NotebookApp.open_browser = False" >> $JUPYTER_CONF
 echo "c.NotebookApp.password = u'sha1:edc66e88705c:7dc477badae478469e3365fa7d8ce609da61a983'" >> $JUPYTER_CONF
 echo "c.NotebookApp.port = 8889" >> $JUPYTER_CONF
