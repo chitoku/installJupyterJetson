d3a20eb66c64c6421b7f87cb34162ea6f8a69175
Chitoku YATO <chitoku@gmail.com>
2018-11-08 18:00:57 +0000

    Take care of libzmq issue

--- installJupyter.sh
+++ installJupyter.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install -y python-pip
+sudo apt-get update
+sudo apt-get install -y python-pip python-dev
 
 # upgrade pip
-pip install --upgrade pip==9.0.3
+sudo -H pip install --upgrade pip==9.0.3
 pip --version	# pip 9.0.1 from /home/ubuntu/.local/lib/python2.7/site-packages (python 2.7)
+
+sudo apt-get install libzmq3-dev
 
 sudo apt-get install -y ipython ipython-notebook
 sudo -H pip install jupyter
